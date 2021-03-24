01bd079af9001fe508750808b1e78340b0882501
Sheraz Khan <upwork1121@gmail.com>
2021-03-24 19:55:09 +0500

    HIW Section Responsive commit 15

--- src/components/HowItWorks/HIWElements.js
+++ src/components/HowItWorks/HIWElements.js
@@ -1,127 +1,125 @@
 import styled from "styled-components"
 import {Link as LinkR} from 'react-router-dom'
-
+import FilledCircle from '../../images/FilledCircle.png'
 export const HIWContainer = styled.div`
-height:600px;
 display:flex;
 flex-direction:column;
-/* justify-content:center; */
 align-items:center;
 background:rgba(10,81,105,.05) 0 0 no-repeat padding-box;
 border:solid 1px;
-
-
-@media screen and (max-width:768px){
-    height:1050px;
-    border:solid 1px;
-}
-@media screen and (min-width:768px) and (max-width:920px){
-     max-height:450px;
-    border:solid 1px;
-}
-/* @media screen and (max-width:480px){
-    height:1200px;
-} */
+max-width:100%;
 `
 
 export const HIWH1 = styled.h1`
+    width:100%;
     font-size: 48px;
     font-weight: 400;
-    line-height: 6;
+    line-height: 3;
     color: #212529;
-    text-align: left;
+    text-align: center;
+    margin:60px;
     @media screen and (min-width:768px) and (max-width:920px){
         font-size: 32px;
 }
     @media screen and (max-width:768px){
-    /* padding:50px; */
     border:solid 1px;
     font-size: 18px;
 }
 `
 export const HIWWapper = styled.div`
-max-width:1000px;
-margin:0 auto;
+border:solid 1px;
 display:grid;
 grid-template-columns: 1fr 1fr 1fr;
 align-items:center;
-grid-gap:16px;
+margin-left:50px;
+margin-right:50px;
 
-@media screen and (max-width:1020px){
-    /* grid-template-columns: 1fr ; */
-    padding:0 50px;
-    
-}
+
 @media screen and (max-width:768px){
     grid-template-columns:1fr;
-    padding:0 20px;
+
     grid-gap:50px;
+    margin-left:0px;
+    margin-right:0px;
 }
 `
 export const HIWCards = styled.div `
 display:flex;
-flex-direction:flex-start;
 align-items:center;
-max-height:700px;
-border:1px solid;
+justify-content:center;
+padding:20px;
 
 @media screen and (max-width:768px){
     flex-direction:column;
-    max-height:800px;
-    
+    padding:0px;   
 }
-
+ 
 
 `
 export const HIWH2 = styled.h1`
-
+    background-image:url(${FilledCircle});
       background-repeat: no-repeat;
     text-align: left;
     background-position: bottom;
     font-weight: 700;
-    font-size: 167px;
+    font-size: 160px;
     letter-spacing: 0;
     color: #0a5169;
     opacity: 1;
     min-height: 143px;
+    font-family: 'M PLUS Rounded 1c', sans-serif;
+    padding:20px;
+
     @media screen and (max-width:768px){
         font-size: 100px;
-        line-height: 120px;
-        background-position: center;
+        line-height: 150px;
+        background-position-y:30px;
+        background-size:100%;
+        width:100%;
+        text-align:center;
+        padding:0px;
+  ;
 
     }
-    @media screen and (min-width:768px) and (max-width:1024px){
+    @media screen and (min-width:768px) and (max-width:920px){
         font-size: 90px;
+        font-weight: 600;
+        padding:0px;
+
+}
+@media screen and (min-width:768px) and (max-width:1120px){
+        font-size: 120px;
         font-weight: 600;
 }
 
 `
-export const HIWP = styled.div`
+export const HIWP = styled.p`
     text-align: left;
     font-size: 12px;
     line-height: 21px;
-    letter-spacing: -.14px;
+    letter-spacing:1px;
     color: #000;
     opacity: 1;
    
-    @media screen and (min-width:768px) {
+    @media screen and (max-width:768px) {
         padding-bottom:80px;
+        text-align:center;
 }
 `
 export const BtnWrapper = styled.div`
 display:flex;
 justify-content:center;
-margin-top:50px;
+margin:20px 0px 40px ;
 `
 export const BtnRoute = styled(LinkR)`
     background: #3ac280 0 0 no-repeat padding-box;
     color:#fff;
     border-color: #3ac280;
-    border-radius: 20px;
-    height: 30px;
-    padding: 7px 25px;
+    padding:12px 40px;
+    border-radius: 30px;
+    text-align:center;
     letter-spacing: 0;
-    font-size: 0.8rem;
+    font-size: 14px;
     line-height: 16px;
     white-space: nowrap;
     min-width: 20px;
